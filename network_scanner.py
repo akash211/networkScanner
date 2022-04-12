543a4aba7b12abd7a51bb7025779a867697e6bd1
Akash Kumar <akash211@gmail.com>
2022-04-12 15:56:14 -0500

    change

--- network_scanner.py
+++ network_scanner.py
@@ -7,10 +7,11 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    print("IP\t\t\t\tMAC Address\n------------------------------------------")
     for element in answered:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
+        print(element[1].psrc, "\t\t\t", element[1].hwsrc)
         print("------------------------")
 
 
