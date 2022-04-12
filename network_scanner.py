9914ecce9b5ed218f0647e2f5598dd113a9c2a99
Akash Kumar <akash211@gmail.com>
2022-04-12 15:52:11 -0500

    change

--- network_scanner.py
+++ network_scanner.py
@@ -6,9 +6,12 @@ import scapy.all as scapy
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    arp_request_broadcast = broadcast / arp_request
+    answered = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    for element in answered:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("------------------------")
 
 
 scan("192.168.136.1/24")
