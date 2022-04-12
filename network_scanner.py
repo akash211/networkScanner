9e75163ed53493284c5525c14f2646ba5fda37b3
Akash Kumar <akash211@gmail.com>
2022-04-12 15:11:08 -0500

    first_save

---
+++ network_scanner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+
+
+def scan(ip):
+    arp_request = scapy.ARP()
+    print(arp_request.summary())
+    scapy.ls(scapy.ARP())
+
+
+scan("192.168.136.1/24")
