503988c2c07c298fb3e865bd0af21dcca56ec096
Akash Kumar <akash211@gmail.com>
2022-04-12 15:25:39 -0500

    change

--- network_scanner.py
+++ network_scanner.py
@@ -4,9 +4,8 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    arp_request = scapy.ARP()
+    arp_request = scapy.ARP(pdst=ip)
     print(arp_request.summary())
-    scapy.ls(scapy.ARP())
 
 
 scan("192.168.136.1/24")
